0829ad532866ad826718399779e5a0cd683938e6
Amirshoja <ashojakhani@gmail.com>
2022-03-10 15:33:17 -0800

    Updated init

--- init.R
+++ init.R
@@ -11,5 +11,5 @@ options(repos=r)
 # packages go here
 install.packages(c('dash', 'readr', 'here', 'ggthemes', 'remotes',
                    'dashCoreComponents', 'dashBootstrapComponents', 
-                   'plotly', 'tidyverse', 'gapminder'))
+                   'plotly', 'tidyverse', 'gapminder', 'ggplot2'))
 remotes::install_github('facultyai/dash-bootstrap-components@r-release')
